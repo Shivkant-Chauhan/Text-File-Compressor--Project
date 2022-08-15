8b9fce7000a948794f73a9ec5e0f0185b1fdd4cd
Shivkant-Chauhan <shivkantchauhan2002@gmail.com>
2022-08-15 17:51:00 +0530

    final working in c++

--- main.cpp
+++ main.cpp
@@ -1,49 +1,115 @@
-/*
-
-make new .cpp files and then include them to this main file..
-just like web dev bruh..!
-
-*/
-
 #include <bits/stdc++.h>
+#include <ext/pb_ds/assoc_container.hpp>
+#include <ext/pb_ds/tree_policy.hpp>
+
 using namespace std;
+using namespace __gnu_pbds;
 
-#define fastio() ios_base::sync_with_stdio(false);cin.tie(NULL);cout.tie(NULL)
+typedef tree<int, null_type, less<int>, //put greater in place of less for storing ordered set in desc order
+        rb_tree_tag, tree_order_statistics_node_update> pbds;
 
-#define pb push_back
-#define ppb pop_back
-#define nln '\n'
-#define ff first
-#define ss second
-#define ins insert
-#define mod 1000000007
-#define set_bits __builtin_popcountll
+        // find_by_order, order_of_key
+//this is Policy-based Data Structure(PBDS)..
+
+
+#define fastio()    ios_base::sync_with_stdio(false);cin.tie(NULL);cout.tie(NULL)
+
+#define pb          push_back
+#define ppb         pop_back
+#define nln         '\n'
+#define ff          first
+#define ss          second
+#define ins         insert
+#define mod         1000000007
+#define YES         cout << "YES";
+#define NO          cout << "NO";
+#define set_bits    __builtin_popcountll
+
 
 typedef long long ll;
 typedef unsigned long long ull;
+typedef long double lld;
+
+/*  --------------------------------------------------------------------------------------------------------------  */
 
 
-
-template <typename T>
+// template ..?
 class TreeNode{
 	public : 
-	T data;
-	TreeNode<T>* left;
-	TreeNode<T>* right;
+	char data;
+    int freq;
+	TreeNode* left;
+	TreeNode* right;
 
-	TreeNode(T data){
+	TreeNode(char data){
 		this->data = data;
 		left = NULL;
 		right = NULL;
 	}
-	~TreeNode(){		// executed when delete root encountered..
-		delete left;	//first delete both its child, then automatically root as executed when delete root is encountered in code
-		delete right;	//not automatically executed as all nodes are dynamically allocated.
+	~TreeNode(){		
+		delete left;
+		delete right;
 	}
 
 };
 
 
+// !!!!!
+// this is the standard way of comparator function for a priority queue
+// then we have to overload the operator function for our operation ...
+class comparator{
+    public: 
+    bool operator()(TreeNode* l, TreeNode* r){
+        return (l->freq > r->freq);
+        // arranging as highest freq items first
+    }
+};
+
+
+// Function to allocate a new tree node
+TreeNode* getNode(char ch, int freq, TreeNode* left, TreeNode* right){
+    TreeNode* node = new TreeNode(ch);
+
+    node->data = ch;
+    node->freq = freq;
+    node->left = left;
+    node->right = right;
+
+    return node;
+}
+
+
+void encode(TreeNode* root, string str, unordered_map<char, string> &huffmanCode){
+    if (root == nullptr)
+        return;
+
+    if (!root->left && !root->right) {
+        huffmanCode[root->data] = str;
+    }
+
+    encode(root->left, str + "0", huffmanCode);
+    encode(root->right, str + "1", huffmanCode);
+}
+
+
+void decode(TreeNode* root, int &id, string str){
+    if(root == nullptr){
+        return;
+    }
+
+    if(!root->left && !root->right){
+        cout << root->data;
+        return;
+    }
+
+    id++;
+
+    if (str[id] == '0')  decode(root->left, id, str);
+    else                 decode(root->right, id, str);
+}
+
+
+/*  --------------------------------------------------------------------------------------------------------------  */
 
 int main(){
     
@@ -52,6 +118,7 @@ int main(){
     #ifndef ONLINE_JUDGE
         freopen("inputf.in", "r", stdin);
         freopen("outputf.in", "w", stdout);
+        freopen("Error.txt", "w", stderr);
     #endif
 
 
@@ -67,6 +134,7 @@ int main(){
     vector<string> input;
 
     string FileName;
+    string inputStr = "";
     // cout << "enter the file name you want to compress" << nln;	//add feature when file location different
     // cin >> FileName;
     FileName = "TEXT_FILE.txt";
@@ -79,64 +147,136 @@ int main(){
 
 		input.pb(temp);
 
+        inputStr += temp;
+        inputStr += " ";
+
 	}
 
 
 //------------------------------------------------------------------------------------
-    //instead of this how can i use min priority queue...?
 
-    map<char, int> hsh;
+    unordered_map<char, ll> freq;
     for(int i = 0; i < input.size(); i++){
     	string temp = input[i];
     	for(int k = 0; k < temp.size(); k++){
-    		if(temp[k] != ' ')
-    			hsh[temp[k]]++;
+    		// if(temp[k] != ' '){
+    			freq[temp[k]]++;
+            // }
     	}
+
     }
 
-
-    multimap<int, char> rev_hsh;
-    for(auto &i : hsh){
-    	// cout << i.first << " " << i.second << nln;
-    	rev_hsh.insert({i.second, i.first});
-    }
+//instead of this i should use priority queue
+    // multimap<ll, char> rev_hsh;
+    // for(auto &i : hsh){
+    // 	// cout << i.first << " " << i.second << nln;
+    // 	rev_hsh.insert({i.second, i.first});
+    // }
 
     // for(auto &i : rev_hsh){
     // 	cout << i.first << " " << i.second << nln;
     // }
 
-    //instead of this how can i use min priority queue...?
+    // queue to store live nodes of Huffman tree;
+    // priority_queue<TreeNode*, vector<TreeNode*>/*, comp*/> pq;
+    priority_queue<TreeNode*, vector<TreeNode*>, comparator> pq;
+
 //------------------------------------------------------------------------------------
 
-    int k = 0;
-    char x;
+//got stucked in the connections of the new nodes.
 
-    while(rev_hsh.size() != 1){
-    	// TreeNode<char> parent, leftCh, rightCh;
-    	auto iter = rev_hsh.begin();
-    	auto min1 = iter;
-    	auto min2 = ++iter;
+//------------------------------------------------------------------------------------
+    //working on TreeNode
 
+    // Create a leaf node for each character and add it to the priority queue.
+    for (auto &x : freq) {
+        pq.push(getNode(x.ff, x.ss, NULL, NULL));
+    }
+    
 
-    	x = '$' + k;
-    	TreeNode<char>* parent = new TreeNode<char>(x);
-    	TreeNode<char>* leftCh = new TreeNode<char>(min1->ff);
-    	TreeNode<char>* rightCh = new TreeNode<char>(min2->ff);
+    while (pq.size() > 1){
+        TreeNode* left = pq.top();
+        pq.pop();
+        TreeNode* right = pq.top();
+        pq.pop();
 
-    	parent->left = leftCh;
-    	parent->right = rightCh;
-
-
-    	rev_hsh.ins({(min1->ff + min2->ff), x});
-    	k++;
-    	rev_hsh.erase(min1);
-    	rev_hsh.erase(min2);
-
+        int sum = left->freq + right->freq;
+        pq.push(getNode('\0', sum, left, right));
     }
 
+    TreeNode* root = pq.top();
+
+    // ll k = 0;
+    // char x;
+
+
+  //   while(rev_hsh.size() != 1){
+
+  //   	TreeNode<char> *parent, *leftCh, *rightCh;
+
+  //   	auto iter = rev_hsh.begin();
+  //   	auto min1 = iter;
+  //   	auto min2 = ++iter;
+
+
+  //   	x = '$' + k;
+  //   	parent = new TreeNode<char>(x);
+		// leftCh = new TreeNode<char>(min1->ff);
+  //   	rightCh = new TreeNode<char>(min2->ff);
+
+  //   	parent->left = leftCh;
+  //   	parent->right = rightCh;
+
+
+  //   	rev_hsh.ins({(min1->ff + min2->ff), x});
+  //   	k++;
+  //   	rev_hsh.erase(min1);
+  //   	rev_hsh.erase(min2);
+
+
+  //   	if(rev_hsh.size() == 1){
+  //   		root = parent;
+  //   	}
+
+
+    // }
+
+
+    // traverse the Huffman Tree and store Huffman Codes
+    // in a map. Also prints them
+    unordered_map<char, string> huffmanCode;
+    encode(root, "", huffmanCode);
+
+    cout << "Huffman Codes are :" << nln << nln;
+    for (auto &z : huffmanCode){
+        cout << z.first << " " << z.second << nln;
+    }
+
+
+    // print encoded string
+    string str = "";
+    for (char ch: inputStr) {
+        str += huffmanCode[ch];
+    }
+
+    cout << "\nEncoded string is :\n" << str << nln;
+
+    int index = -1;
+    cout << "\nDecoded string is: " << nln;
+    while (index < (int)str.size() - 2){
+        decode(root, index, str);
+    }
+
+
+
 	// cout << rev_hsh.begin()->ff;
+//------------------------------------------------------------------------------------
 
 
+//------------------------------------------------------------------------------------
+    // how to traverse on that tree..?
+
+// cout << root->left->left->data;
 
 
     return 0;
